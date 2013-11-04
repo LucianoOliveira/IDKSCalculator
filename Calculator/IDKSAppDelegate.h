3c66117425e4b8981853a287c3f2ea611d8c8483
LucianoOliveira <Luciano@lucianos-air.lan>
2013-11-04 18:41:07 +0000

    Initial Commit

---
+++ Calculator/IDKSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  IDKSAppDelegate.h
+//  Calculator
+//
+//  Created by Luciano Oliveira on 04/11/13.
+//  Copyright (c) 2013 IDKS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IDKSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Calculator/IDKSViewController.h
@@ -0,0 +1,13 @@
+//
+//  IDKSViewController.h
+//  Calculator
+//
+//  Created by Luciano Oliveira on 04/11/13.
+//  Copyright (c) 2013 IDKS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IDKSViewController : UIViewController
+
+@end
